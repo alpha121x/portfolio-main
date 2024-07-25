0fd203b2cc00f6fdec1419249d3bd944c6c9749e
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-07-26 01:18:50 +0500

    edited hero and footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Abbas, a Next.js Developer based in Pakistan.
           </p>
 
           <a href="#about">
