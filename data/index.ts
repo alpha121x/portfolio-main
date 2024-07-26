a73433d3ea2a9e07785afc595c877c843ec6aa81
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-07-26 12:36:32 +0500

    updates

--- data/index.ts
+++ data/index.ts
@@ -173,22 +173,22 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "PHP Devloper Intern",
+    desc: "Assisted in the development of a web-based platform using PHP,Mysql, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Web App Dev - Next-gen Tech",
+    desc: "Designed and developed web app for both desktop & Android platforms using custom code.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Freelance Web Dev Project",
+    desc: "Led the dev of a web app for a client, from initial concept to deployment on custom domain.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
