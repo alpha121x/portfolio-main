09860ec2f40c7418cb2a1ba45859a57fc4e6772a
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-07-26 16:03:45 +0500

    addition code

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -36,7 +36,6 @@ const Clients = () => {
                 />
                 <img
                   src={company.nameImg}
-                  alt={company.name}
                   width={company.id === 4 || company.id === 5 ? 100 : 150}
                   className="md:w-24 w-20"
                 />
--- data/index.ts
+++ data/index.ts
@@ -140,33 +140,27 @@ export const testimonials = [
 export const companies = [
   {
     id: 1,
-    name: "cloudinary",
-    img: "/cloud.svg",
-    nameImg: "/cloudName.svg",
+    name: "bixisoft",
+    img: "/bixi.svg",
+    nameImg: "/bixisoftName.svg",
   },
   {
     id: 2,
-    name: "appwrite",
-    img: "/app.svg",
-    nameImg: "/appName.svg",
+    name: "debtavisor",
+    img: "/debt.svg",
+    nameImg: "/debtavisorName.svg",
   },
   {
     id: 3,
-    name: "HOSTINGER",
-    img: "/host.svg",
-    nameImg: "/hostName.svg",
+    name: "EstateAgency",
+    img: "/estate.svg",
+    nameImg: "/E-Agency.svg",
   },
   {
     id: 4,
-    name: "stream",
-    img: "/s.svg",
-    nameImg: "/streamName.svg",
-  },
-  {
-    id: 5,
-    name: "docker.",
-    img: "/dock.svg",
-    nameImg: "/dockerName.svg",
+    name: "totaldebtpro",
+    img: "/totaldebt.svg",
+    nameImg: "/debtproName.svg",
   },
 ];
 
