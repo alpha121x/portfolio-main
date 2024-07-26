626fe7b337ee0fb1a5d478efe71334ebb090118b
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-07-26 13:22:34 +0500

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Abbas, a Next.js Developer based in Pakistan.
+            Hi! I&apos;m Muhammad Abbas, a Professional Web Developer.
           </p>
 
           <a href="#about">
