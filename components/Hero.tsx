4677c2a381fb0e1e187ef5f53c85b81a7165b985
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-08-12 01:59:36 +0500

    updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,9 +43,9 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <img
-            src="/logo-robx.png"
+            src="/logo-new.png"
             alt="RobX Solutions Logo"
-            className="mx-auto max-w-80"
+            className="mx-auto max-w-80 opacity-0.1"
           />
 
           {/**
