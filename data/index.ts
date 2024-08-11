fd6889f86ee00395e1014726da502675f58e6a68
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-08-12 00:45:40 +0500

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -18,7 +18,7 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "I'm very flexible with time zone communications",
+    title: "We're very flexible with time zone communications",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
