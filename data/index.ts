2af66f85377138328eec287397e40b30c90cd9d8
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-08-12 02:30:46 +0500

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -28,7 +28,7 @@ export const gridItems = [
   },
   {
     id: 3,
-    title: "My tech stack",
+    title: "Our tech stack",
     description: "We constantly try to improve",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
@@ -38,7 +38,8 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Tech enthusiast with a passion for development.",
+    title:
+      "Turning your innovative ideas into reality with a team of passionate IT experts.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -49,7 +50,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently working on Next.JS & React.JS",
+    title: "Currently working on Shopify services & vitual assistence.",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
