fa7fb2ed982274b7158d6bff44d4975f20a507c5
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-08-12 01:30:29 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Abbas's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "RobX Solutions",
+  description: "Information Technology Company",
 };
 
 export default function RootLayout({
